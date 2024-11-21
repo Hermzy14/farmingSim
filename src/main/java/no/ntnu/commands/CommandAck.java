fb0811de088ce2cc6a6642f1db0f52cc6d934d00
Herman Lundby-Holen <hermanluho@gmail.com>
2024-11-21 10:51:41 +0100

    Created command ack class.

---
+++ src/main/java/no/ntnu/commands/CommandAck.java
@@ -0,0 +1,27 @@
+package no.ntnu.commands;
+
+/**
+ * Command to acknowledge a command. This is sent from a node to the control panel node to acknowledge
+ * that a command has been received and processed.
+ */
+public class CommandAck {
+  private final int commandId;
+
+  /**
+   * Command to acknowledge a command.
+   *
+   * @param commandId The ID of the command to acknowledge
+   */
+  public CommandAck(int commandId) {
+    this.commandId = commandId;
+  }
+
+  /**
+   * Get the ID of the command to acknowledge.
+   *
+   * @return The ID of the command.
+   */
+  public int getCommandId() {
+    return commandId;
+  }
+}
