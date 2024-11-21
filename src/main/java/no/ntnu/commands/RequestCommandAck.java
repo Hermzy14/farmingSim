e37e4de049d7715e20f9613aaa0833103cab417b
Herman Lundby-Holen <hermanluho@gmail.com>
2024-11-21 10:49:48 +0100

    Created request command acknowledgement class.

---
+++ src/main/java/no/ntnu/commands/RequestCommandAck.java
@@ -0,0 +1,29 @@
+package no.ntnu.commands;
+
+/**
+ * Command to request an acknowledgment for a command.
+ *
+ * <p>When a control panel node wants to request an acknowledgment for a command, it sends this
+ * command to the node.
+ */
+public class RequestCommandAck implements Command {
+  private final int commandId;
+
+  /**
+   * Command to request an acknowledgment for a command.
+   *
+   * @param commandId The ID of the command to request an acknowledgment for
+   */
+  public RequestCommandAck(int commandId) {
+    this.commandId = commandId;
+  }
+
+  /**
+   * Get the ID of the command to request an acknowledgment for.
+   *
+   * @return The ID of the command.
+   */
+  public int getCommandId() {
+    return commandId;
+  }
+}
